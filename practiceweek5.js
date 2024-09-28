86c5faa2493a81740b34125ca44ddd407b0c2bee
Tykwon <Dyn721@aol.com>
2024-09-28 14:47:22 -0400

    file with no name spaces

---
+++ practiceweek5.js
@@ -0,0 +1,61 @@
+// // Ask the user for input
+// let age = prompt("What is your Age")
+
+// // Age verification checker logic
+// if (age < 18) {
+//     alert('you are a mynah')
+// } 
+// else if (age >= 18 && age <= 60) {
+//     alert('You are an adult')
+// }
+// else {
+//     alert('I respect you, senior!')
+// }
+
+// // print the logic
+// // console.log(user)
+// // Simple Age Verification App!
+
+// Conditional grade evaluator!
+
+ let score = prompt("What is the participant's score?")
+
+//No invalid negative scores allowed
+ if (score<0) {
+     score = prompt ("Please enter a valid score. We don't give negative grades.") 
+ }
+
+ if (score>=90 ) {
+     console.log('You scored an A! How good')
+ }
+ else if (score<90 && score >=80) {
+     console.log('Enjoy your B score!')
+ }
+ else if (score<80 && score >=70) {
+     console.log('You earned a C. You made it.')
+ }
+ else {
+     console.log('You did not pass this test')
+ }
+
+//Introduction Function! Also, comment out code is highlight, cmd + ?
+// function introduction (name, age) {
+//     console.log("Hello, my name is" + name + ". I am" + age + "years old.")
+// }
+
+// // Ask the user for input
+// let isThisTrue = false;
+
+// let checkforFalse = 0;
+
+// let myName = prompt('What is your name');
+// let age = prompt('What is your age');
+
+// function introduction (myName, age) {
+    //alert('The introduction function is running')
+//     alert('Greetles! My name is ' + myName + ' and I have lived for ' + age)
+// }
+
+// calling the function
+// introduction(myName, age)
+
